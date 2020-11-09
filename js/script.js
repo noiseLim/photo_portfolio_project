38184f079800ac4b98058f4dbe862afd065afbf4
noiseLIm <noiselim@gmail.com>
2020-11-09 17:40:04 +0300

    work on slider

--- js/script.js
+++ js/script.js
@@ -1,169 +1,143 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-        // studio cards
+    // studio cards
 
-        class StudioCards {
-            constructor (src, alt, title, descr, price, parentSelector, ...classes) {
-                this.src = src;
-                this.alt = alt;
-                this.title = title;
-                this.descr = descr;
-                this.price = price;
-                this.classes = classes;
-                this.parent = document.querySelector(parentSelector);
-            }
-    
-            render() {
-                const element = document.createElement('div');
-    
-                if (this.classes.length === 0) {
-                    this.classes = 'studio__item';
-                    element.classList.add(this.classes);
-                } else {
-                    this.classes.forEach(className => element.classList.add(className));
-                }
-    
-                element.innerHTML = `
-                    <img data-foto src=${this.src} alt=${this.alt}>
-                    <h3 class="studio__item-subtitle">${this.title}</h3>
-                    <div class="studio__item-descr">${this.descr}</div>
-                    <div class="studio__item-divider"></div>
-                    <div class="studio__item-price">
-                        <div class="studio__item-cost">Price:</div>
-                        <div class="studio__item-total"><span>${this.price}</span> rub/hour</div>
-                    </div>
-                `;
-    
-                this.parent.append(element);
-    
-            }
+    class StudioCards {
+        constructor (src, alt, title, descr, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
         }
-    
-        new StudioCards(
-            "images/A170_2.jpg",
-            "A170",
-            'Studio A170',
-            "170 m2<br>Cyclorama 8х5 m<br>Power connector 32А<br>Power connector 63А<br>Make-up artist's workplace<br>4 x Profoto D1",
-            3000,
-            '.container_studio',
-        ).render();
-        
-        new StudioCards(
-            "images/A120_4.jpg",
-            "A120",
-            'Studio A120',
-            "A room with white walls and a large cyclorama is a classic solution for a photo studio.<br><br>120 m2<br>Circular cyclorama 6x7 m<br>4 x Profoto D1<br>Ceilings 5m<br>Windows along the entire length of the hall<br>Spacious dedicated space for makeup artist work<br>Power connector 32A<br>Grunge wall, which effectively contrasts with the general interior of the hall<br>Audio system",
-            2200,
-            '.container_studio',
-        ).render();
-    
-        new StudioCards(
-            "images/A85_5.jpg",
-            "A85",
-            'Studio A185',
-            "85 m2<br>Cyclorama 5x5 m<br>Profoto<br>Dark wood floors<br>Power connector 32А<br>Dedicated dressing area<br>Grunge wall from the time of the building<br>3 x Profoto D1",
-            1700,
-            '.container_studio',
-        ).render();
-    
-    //tabs
-    
-    const tabsParent = document.querySelector('.tabheader__items'),
-          tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent');
 
-    function hideTabContent() {
-        tabsContent.forEach(item => {
-            item.classList.add('hide');
-            item.classList.remove('show', 'fade');
+        render() {
+            const element = document.createElement('div');
 
-        });
-
-        tabs.forEach(item => {
-            item.classList.remove('tabheader__item_active');
-        });
-    }
-
-    function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active');
-    }
-        
-    hideTabContent();
-    showTabContent();
-
-    tabsParent.addEventListener('click', (e) => {
-        const target = e.target;
-
-        if (target && target.classList.contains('tabheader__item')) {
-            tabs.forEach((item, i) => {
-                if (target == item) {
-                    hideTabContent();
-                    showTabContent(i);
-                }
-            });
-        }
-    });
-
-    // timer
-
-    const deadline = '2020-12-31';
-
-    function getTimeRemaining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-              days = Math.floor(t / (1000 * 60 * 60 * 24)),
-              hours = Math.floor(t / (1000 * 60 * 60) % 24),
-              minutes = Math.floor(t /(1000 * 60) % 60),
-              seconds = Math.floor((t / 1000) % 60);
-
-        return {
-            'total': t,
-            'days': days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-        };
-    }
-    
-    function getZero(num) {
-        if (num >= 0 && num < 10) {
-            return `0${num}`;
-        } else {
-            if (num <= 0) {
-                return `00`;
+            if (this.classes.length === 0) {
+                this.classes = 'studio__item';
+                element.classList.add(this.classes);
             } else {
-                return num;
+                this.classes.forEach(className => element.classList.add(className));
             }
-        } 
+
+            element.innerHTML = `
+                <img data-foto src=${this.src} alt=${this.alt}>
+                <h3 class="studio__item-subtitle">${this.title}</h3>
+                <div class="studio__item-descr">${this.descr}</div>
+                <div class="studio__item-divider"></div>
+                <div class="studio__item-price">
+                    <div class="studio__item-cost">Price:</div>
+                    <div class="studio__item-total"><span>${this.price}</span> rub/hour</div>
+                </div>
+            `;
+
+            this.parent.append(element);
+
+        }
     }
 
-    function setClock(selector, endtime) {
-        const timer = document.querySelector(selector),
-              days = timer.querySelector('#days'),
-              hours = timer.querySelector('#hours'),
-              minutes = timer.querySelector('#minutes'),
-              seconds = timer.querySelector('#seconds'),
-              timeInterval = setInterval(updateClock, 1000);
+    new StudioCards(
+        "images/A170_2.jpg",
+        "A170",
+        'Studio A170',
+        "170 m2<br>Cyclorama 8х5 m<br>Power connector 32А<br>Power connector 63А<br>Make-up artist's workplace<br>4 x Profoto D1",
+        3000,
+        '.container_studio',
+    ).render();
+    
+    new StudioCards(
+        "images/A120_4.jpg",
+        "A120",
+        'Studio A120',
+        "A room with white walls and a large cyclorama is a classic solution for a photo studio.<br><br>120 m2<br>Circular cyclorama 6x7 m<br>4 x Profoto D1<br>Ceilings 5m<br>Windows along the entire length of the hall<br>Spacious dedicated space for makeup artist work<br>Power connector 32A<br>Grunge wall, which effectively contrasts with the general interior of the hall<br>Audio system",
+        2200,
+        '.container_studio',
+    ).render();
 
-        updateClock();
+    new StudioCards(
+        "images/A85_5.jpg",
+        "A85",
+        'Studio A185',
+        "85 m2<br>Cyclorama 5x5 m<br>Profoto<br>Dark wood floors<br>Power connector 32А<br>Dedicated dressing area<br>Grunge wall from the time of the building<br>3 x Profoto D1",
+        1700,
+        '.container_studio',
+    ).render();
 
-        function updateClock() {
-            const t = getTimeRemaining(endtime);
 
-            days.innerHTML = getZero(t.days);
-            hours.innerHTML = getZero(t.hours);
-            minutes.innerHTML = getZero(t.minutes);
-            seconds.innerHTML = getZero(t.seconds);
+    // slider foto
 
-            if (t.total <= 0) {
-                clearInterval(timeInterval);
-            }
+    class SliderCards {
+        constructor (wrapper, alt, parentSelector, ...src) {
+            this.wrapper = wrapper;
+            this.src = src;
+            this.alt = alt;
+            // this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
         }
 
+        render() {
+            const element = document.createElement('div');
+
+            // if (this.classes.length === 0) {
+            //     this.classes = 'studio__slider-inner';
+            //     element.classList.add(this.classes);
+            // } else {
+            //     this.classes.forEach(className => element.classList.add(className));
+            // }
+
+            // this.src.forEach(item => element.classList.add(item));
+
+            element.classList.add('studio__slider-inner');
+
+            this.src.forEach(item => {
+                console.log(element.innerHTML = `
+                    <div class=${this.wrapper}>
+                        <img src=${item} alt=${this.alt}>
+                    </div>
+                `);
+            });
+
+            console.log(this.src);
+
+            // element.classList.add('studio__slider-inner');
+            // element.innerHTML = `
+            //     <div class=${this.wrapper}>
+            //         <img src=${this.src} alt=${this.alt}>
+            //     </div>
+            //     <div class=${this.wrapper}>
+            //         <img src=${this.src} alt=${this.alt}>
+            //     </div>
+            //     <div class=${this.wrapper}>
+            //         <img src=${this.src} alt=${this.alt}>
+            //     </div>
+            //     <div class=${this.wrapper}>
+            //         <img src=${this.src} alt=${this.alt}>
+            //     </div>
+            //     <div class=${this.wrapper}>
+            //         <img src=${this.src} alt=${this.alt}>
+            //     </div>
+                
+            // `;
+            this.parent.append(element);
+        }
     }
 
-    setClock('.timer', deadline);
+    new SliderCards(
+        "studio__slide",
+        "FotoStudio_A170",
+        ".studio__slider-wrapper",
+        "images/studio_A170/A170.jpg",
+        "images/studio_A170/A170_2.jpg",
+        "images/studio_A170/A170_3.jpg",
+        "images/studio_A170/A170_4.jpg",
+        "images/studio_A170/A170_5.jpg",
+    ).render();
+
+   
+
 
     // modal studio
 
@@ -348,6 +322,137 @@ window.addEventListener('DOMContentLoaded', () => {
             nilDecoration();
             opacityMode();
         });
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // timer
+
+    const deadline = '2020-12-31';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+                days = Math.floor(t / (1000 * 60 * 60 * 24)),
+                hours = Math.floor(t / (1000 * 60 * 60) % 24),
+                minutes = Math.floor(t /(1000 * 60) % 60),
+                seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+    
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            if (num <= 0) {
+                return `00`;
+            } else {
+                return num;
+            }
+        } 
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+                days = timer.querySelector('#days'),
+                hours = timer.querySelector('#hours'),
+                minutes = timer.querySelector('#minutes'),
+                seconds = timer.querySelector('#seconds'),
+                timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+    }
+
+    setClock('.timer', deadline);
+
+    //tabs
+    
+    const tabsParent = document.querySelector('.tabheader__items'),
+          tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+        
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
     });
 
 
